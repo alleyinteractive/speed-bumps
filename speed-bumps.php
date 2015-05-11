64f2b286b884ff382b9480d826b25a0d4d9411b1
Daniel Bachhuber <d@danielbachhuber.com>
2015-05-11 08:19:31 -0700

    Initial commit

---
+++ speed-bumps.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: Speed-bumps
+Version: 0.1-alpha
+Description: PLUGIN DESCRIPTION HERE
+Author: YOUR NAME HERE
+Author URI: YOUR SITE HERE
+Plugin URI: PLUGIN SITE HERE
+Text Domain: speed-bumps
+Domain Path: /languages
+*/
+
