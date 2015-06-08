d55965ca924be8e891aca8b944891ddee8353ffd
Noppanit Charassinvichai <noppanit.c@gmail.com>
2015-06-08 16:15:20 -0400

    Flip the logic of dummy constraint

--- inc/element-constraints/class-speed-bumps-dummy-constraint.php
+++ inc/element-constraints/class-speed-bumps-dummy-constraint.php
@@ -3,6 +3,6 @@ namespace Speed_Bumps\Constraint\Element;
 
 class Speed_Bumps_Dummy_Constraint extends Speed_Bumps_Element_Constraint {
 	public function contains( $paragraph ) {
-		return false;
+		return true;
 	}
 }
