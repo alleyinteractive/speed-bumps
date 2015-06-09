aa5778f911ff360932a806c5ba530c492e9e0291
=Anthony Rodriguez <AnthonyRodriguez.itt@gmail.com>
2015-06-09 12:15:17 -0400

    Removing unnecessary annotaions

--- speed-bumps.php
+++ speed-bumps.php
@@ -1,11 +1,4 @@
 <?php
-// anthony2727/speed-bumps
-// forked from fusioneng/speed-bumps
-// AnthonyRodriguez.itt@gmail.com
-// www.github.com/anthony2727
-//  
-//  
-
 /*
 Plugin Name: Speed-bumps
 Version: 0.1-alpha
@@ -17,18 +10,11 @@ Text Domain: speed-bumps
 Domain Path: /languages
 */
 
-
-//NO necesary to manually include the SpeedBumps class anymore. 
-//Dependencies are handled by the root level loader.php file
-// 
-//require_once dirname( __FILE__ ) . '/inc/class-speed-bumps.php';
-//
-//
 //Only the 'loader.php' file will be included within the whole project.  
 require_once dirname (__FILE__) . '/loader.php';
+
 // @codingStandardsIgnoreStart
 function Speed_Bumps() {
-	//Calling the SpeedBumps class from the respective namespace
 	return \SpeedBumps\Inc\SpeedBumps::get_instance();
 }
 // @codingStandardsIgnoreEnd
