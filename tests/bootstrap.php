048537dc42d5321b9e3e367657dc77aa1ff2dd6e
Noppanit Charassinvichai <noppanit.c@gmail.com>
2015-06-16 13:15:19 -0400

    Revert "Debuggin travis"
    This reverts commit 34bc92730978debe6498fa3b2544dfa4d82c3d64.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,7 +5,7 @@ if ( ! $_tests_dir ) {
 	$_tests_dir = '/tmp/wordpress-tests-lib';
 }
 
-require_once $_tests_dir . '/functions.php';
+require_once $_tests_dir . '/includes/functions.php';
 
 function _manually_load_plugin() {
 	require dirname( __FILE__ ) . '/../speed-bumps.php';
