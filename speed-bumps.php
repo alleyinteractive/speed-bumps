0bf2dd7c17c36a61fe359977cb6f05c695b9d3fa
Nathaniel Taintor <goldenapplesdesign@gmail.com>
2015-06-16 14:35:26 -0700

    Comment autoloader function & move it to the main file
    Slightly modifies the autoloader function for readability, adds docblock
    comment explaining it, and moves it to the main plugin file.
    
    This autoloader should be able to be used as part of a template for all
    new plugin projects; it'd be nice if it looked closer in style to our
    coding structure.

--- speed-bumps.php
+++ speed-bumps.php
@@ -10,13 +10,57 @@ Plugin URI: https://github.com/fusioneng/speed-bumps
 Text Domain: speed-bumps
 Domain Path: /languages
 */
-//Load any class-type file dependency automatically.  
-require_once dirname (__FILE__) . '/loader.php';
 
 // @codingStandardsIgnoreStart
 function Speed_Bumps() {
+	require_once( 'inc/class-speed-bumps.php' );
 	return Speed_Bumps::get_instance();
 }
 // @codingStandardsIgnoreEnd
+
 add_action( 'init', 'Speed_Bumps' );
+
+
+/**
+ * Register an autoloader function for any class in the plugin's namespace.
+ *
+ * If a class referenced is within the 'Speed_Bumps' namespace, it will be
+ * autoloaded from a file name matching the class namespace and name, as
+ * adjusted to follow WordPress naming conventions:
+ * https://make.wordpress.org/core/handbook/coding-standards/php/#naming-conventions
+ *
+ * Specifically, the following transforms will be applied:
+ *
+ * - The top-level "Speed_Bumps" prefix will be removed,
+ * - each portion of the namespace hierarchy will be downcased & transformed into harpoon-case,
+ * - and used as a path segment of a directory within the `/inc/` directory,
+ * - and the file containing the class itself will be named `class-{classname}.php`
+ */
+spl_autoload_register(
+	function ($class) {
+
+		// project-specific namespace
+		$prefix = 'Speed_Bumps';
+
+		$parts = explode( '\\', $class );
+
+		if ( $parts[0] !== $prefix ) {
+			return;
+		}
+
+		array_shift( $parts );
+
+		$last = array_pop( $parts ); // File should be 'class-[...].php'
+		$last = 'class-' . $last . '.php';
+
+		$parts[] = $last;
+		$file = dirname( __FILE__ ) . '/inc/' . str_replace( '_', '-', strtolower( implode( $parts, '/' ) ) );
+
+		//If the file exists....
+		if ( file_exists( $file ) ) {
+			//Require the file
+			require($file);
+		}
+	}
+);
 
