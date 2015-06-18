b138b678b1a1fdd398da3f46e745a24062a85056
Nathaniel Taintor <goldenapplesdesign@gmail.com>
2015-06-18 15:35:37 -0700

    Adds ad_already_inserted and already_inserted_here constraints
    One change to the way speed bumps are registered: adds the 'id' field to
    the args array so it can been checked in insertion checks.
    
    Breaks out the insertion constraints logic into two separate functions:
    has this particular speed bump already been inserted? and has another
    speed bump been inserted at this location?
    
    Updates tests to cover these two methods.

--- inc/constraints/content/class-injection.php
+++ inc/constraints/content/class-injection.php
@@ -3,24 +3,33 @@
 namespace Speed_Bumps\Constraints\Content;
 
 class Injection {
+
+	/**
+	 * Has this particular speedbump already been inserted?
+	 *
+	 */
 	public static function did_already_insert_ad( $can_insert, $context, $args, $already_inserted ) {
-		if ( self::is_ad_already_inserted_here( $context, $already_inserted ) ) {
+		if ( in_array( $args['id'], wp_list_pluck( $already_inserted, 'speed_bump_id' ) ) ) {
 			$can_insert = false;
 		}
 
 		return $can_insert;
 	}
 
-	private static function is_ad_already_inserted_here( $context, $already_inserted ) {
+	/**
+	 * Has another speed bump been inserted at this index?
+	 *
+	 */
+	public static function is_ad_already_inserted_here( $can_insert, $context, $args, $already_inserted ) {
 		$current_index = $context['index'];
+
 		foreach ( $already_inserted as $index => $element ) {
 			if ( $element['index'] === $current_index ) {
-				return true;
-				break;
+				$can_insert = false;
 			}
 		}
 
-		return false;
+		return $can_insert;
 	}
 
 }
--- tests/test-speed-bumps-injection-constraints.php
+++ tests/test-speed-bumps-injection-constraints.php
@@ -13,17 +13,42 @@ class Test_Speed_Bumps_Injection_Constraints extends WP_UnitTestCase {
 				'inserted_content' => 'content1',
 			),
 		);
-		$ad_did_inserted = \Speed_Bumps\Constraints\Content\Injection::did_already_insert_ad( true, array( 'index' => 0 ), array(), $already_inserted );
+		$ad_did_inserted = \Speed_Bumps\Constraints\Content\Injection::did_already_insert_ad( true, array( 'index' => 2 ), array( 'id' => 'speed_bump1' ), $already_inserted );
 
 		$this->assertFalse( $ad_did_inserted );
 	}
 
 	public function test_ad_not_already_inserted() {
-		$ad_did_not_insert = \Speed_Bumps\Constraints\Content\Injection::did_already_insert_ad( true, array( 'index' => 0 ), array(), array() );
+		$already_inserted = array();
+		$ad_did_not_insert = \Speed_Bumps\Constraints\Content\Injection::did_already_insert_ad( true, array( 'index' => 0 ), array( 'id' => 'speed_bump1' ), array() );
 
 		$this->assertTrue( $ad_did_not_insert );
 
 	}
 
+	public function test_ad_already_inserted_here() {
+		$already_inserted = array(
+			array(
+				'index' => 1,
+				'speed_bump_id' => 'speed_bump1',
+				'inserted_content' => 'content1',
+			),
+		);
+		$ad_did_inserted = \Speed_Bumps\Constraints\Content\Injection::is_ad_already_inserted_here( true, array( 'index' => 1 ), array( 'id' => 'apeed_bump2' ), $already_inserted );
 
+		$this->assertFalse( $ad_did_inserted );
+	}
+
+	public function test_ad_not_already_inserted_here() {
+		$already_inserted = array(
+			array(
+				'index' => 0,
+				'speed_bump_id' => 'speed_bump1',
+				'inserted_content' => 'content1',
+			),
+		);
+		$ad_did_inserted = \Speed_Bumps\Constraints\Content\Injection::is_ad_already_inserted_here( true, array( 'index' => 1 ), array( 'id' => 'apeed_bump2' ), $already_inserted );
+
+		$this->assertTrue( $ad_did_inserted );
+	}
 }
