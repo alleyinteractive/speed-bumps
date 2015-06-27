acece3d95e469daa46ad0076263732a661f1634b
Nathaniel Taintor <goldenapplesdesign@gmail.com>
2015-06-27 17:44:19 -0600

    Add "distance from end of article" constraints
    Adds two new constraints to the basic set of filters available:
    
    - 'meets_minimum_distance_from_article_end_paragraphs': will ensure that
      a speed bump is not inserted if its not the minimum number of
      paragraphs from the article end.
    - 'meets_minimum_distance_from_article_end_words': will ensure that
      a speed bump is not inserted if its not the minimum number of
      words from the article end.
    
    I didn't set default arg values for either of these in
    register_speed_bump, so they will only be used if the user passes one
    of these values on registering the array.

--- inc/constraints/text/class-minimum-text.php
+++ inc/constraints/text/class-minimum-text.php
@@ -12,4 +12,48 @@ class Minimum_Text {
 		return $can_insert;
 	}
 
+	/**
+	 * Is this point far enough from the end to insert, counting by paragraphs?
+	 *
+	 * Blocks insertion if the current insertion point is less than the minimum
+	 * paragraphs from end value, if "minimum_paragraphs_from_end" is defined
+	 * in the speed bump registration arguments.
+	 */
+	public static function meets_minimum_distance_from_article_end_paragraphs( $can_insert, $context, $args, $already_inserted ) {
+		if ( ! isset( $args['minimum_paragraphs_from_end'] ) ) {
+			return $can_insert;
+		}
+		$paragraphs_from_end = $context['total_paragraphs'] - $context['index'] - 1;
+		if ( $paragraphs_from_end < intval( $args['minimum_paragraphs_from_end'] ) ) {
+			$can_insert = false;
+		}
+
+		return $can_insert;
+	}
+
+	/**
+	 * Is this point far enough from the end to insert, counting by words?
+	 *
+	 * Blocks insertion if the current insertion point is less than the minimum
+	 * number of words from end value, if "minimum_words_from_end" is defined
+	 * in the speed bump registration arguments.
+	 *
+	 * Used in conjunction with `meets_minimum_distance_from_article_end_paragraphs`, 
+	 * to account for cases where very short paragraphs are used.
+	 */
+	public static function meets_minimum_distance_from_article_end_words( $can_insert, $context, $args, $already_inserted ) {
+		if ( ! isset( $args['minimum_words_from_end'] ) ) {
+			return $can_insert;
+		}
+		
+		$remaining_paragraphs = array_slice( $context['parts'], $context['index'] );
+		$remaining_text = implode( ' ', $remaining_paragraphs );
+		$remaining_words = array_filter( explode( ' ', $remaining_text ) );
+
+		if ( count( $remaining_words ) < intval( $args['minimum_words_from_end'] ) ) {
+			$can_insert = false;
+		}
+
+		return $can_insert;
+	}
 }
