fcd27396c1bc74edb55cc36c343df160377c5ff6
Nathaniel Taintor <goldenapplesdesign@gmail.com>
2015-07-06 17:20:02 -0500

    Update tests for speed bump registration
    Updates tests to use new args structure where applicable.

--- tests/test-speed-bumps-registration.php
+++ tests/test-speed-bumps-registration.php
@@ -25,19 +25,20 @@ class Test_Speed_Bumps_Registration extends WP_UnitTestCase {
 		$speed_bump1_args = $this->speed_bumps->get_speed_bump( 'speed_bump1' );
 
 		$this->assertEquals( call_user_func( $speed_bump1_args['string_to_inject'] ), '' );
-		$this->assertEquals( $speed_bump1_args['from_element'], array( 'paras' => 1, 0 => 'iframe', 1 => 'oembed', 'image' => array( 'paras' => 2 ) ) );
-		$this->assertEquals( $speed_bump1_args['paragraph_offset'], 0 );
+
+		$expected_args = array( 'paragraphs' => 1, 0 => 'iframe', 1 => 'oembed', 'image' => array( 'paragraphs' => 2 ) );
+		$this->assertEquals( $expected_args, $speed_bump1_args['from_element'] );
 	}
 
 	public function test_speed_bump_registration_with_different_arguments() {
 		$this->speed_bumps->register_speed_bump( 'speed_bump1' );
 		$speed_bump1_args = $this->speed_bumps->get_speed_bump( 'speed_bump1' );
 
-		$this->assertEquals( $speed_bump1_args['minimum_content_length'], array( 'paras' => 8, 'chars' => 1200 ) );
+		$this->assertEquals( $speed_bump1_args['minimum_content_length'], array( 'paragraphs' => 8, 'characters' => 1200 ) );
 
-		$this->speed_bumps->register_speed_bump( 'speed_bump2', array( 'minimum_content_length' => 10 ) );
+		$this->speed_bumps->register_speed_bump( 'speed_bump2', array( 'minimum_content_length' => array( 'paragraphs' => 10 ) ) );
 		$speed_bump2_args = $this->speed_bumps->get_speed_bump( 'speed_bump2' );
-		$this->assertEquals( $speed_bump2_args['minimum_content_length'], 10 );
+		$this->assertEquals( $speed_bump2_args['minimum_content_length'], array( 'paragraphs' => 10 ) );
 	}
 
 }
