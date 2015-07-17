d39538f929f3c179d24c4df656997ed38b8b22ae
Noppanit Charassinvichai <noppanit.c@gmail.com>
2015-07-17 11:22:24 -0400

    Deleted unused arguments

--- tests/test-speed-bumps-integration.php
+++ tests/test-speed-bumps-integration.php
@@ -120,13 +120,12 @@ class Test_Speed_Bumps_Integration extends WP_UnitTestCase {
 		\Speed_Bumps()->register_speed_bump( 'speed_bump1', array(
 			'string_to_inject' => function() { return 'test1'; },
 			'minimum_content_length' => 1,
-			'paragraph_offset' => 0,
+			'from_start' => 0
 		) );
 
 		\Speed_Bumps()->register_speed_bump( 'speed_bump2', array(
 			'string_to_inject' => function() { return 'test2'; },
 			'minimum_content_length' => 1,
-			'paragraph_offset' => 0,
 		) );
 
 		Speed_Bumps()->clear_speed_bump( 'speed_bump2' );
