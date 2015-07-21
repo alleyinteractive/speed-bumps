d65c08da08ca4f5c67b8312fb1f60412287b15ce
Daniel Bachhuber <d@danielbachhuber.com>
2015-07-21 06:38:32 -0700

    Use `WP_Embed::shortcode()` to ensure we hit cache
    This also means we will hit expected filters

--- inc/constraints/elements/class-oembed.php
+++ inc/constraints/elements/class-oembed.php
@@ -6,7 +6,7 @@ class Oembed extends Constraint_Abstract {
 	public function paragraph_not_contains_element( $paragraph ) {
 		preg_match_all( '|^\s*(https?://[^\s"]+)\s*$|im', $paragraph, $matches );
 		foreach ( $matches[1] as $match ) {
-			if ( wp_oembed_get( $match ) ) {
+			if ( $GLOBALS['wp_embed']->shortcode( array(), $match ) ) {
 				return false;
 			}
 		}
